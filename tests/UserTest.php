de15b3aaf63ce67d9c247c81d637b6e4f2fc9ca8
Jorge Borgos <jorge__b@hotmail.com>
2022-12-09 16:10:02 -0500

    first commit

---
+++ tests/UserTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use HW\User;
+
+final class UserTest extends TestCase
+{
+    public function testClassConstructor()
+    {
+        $user = new User(18, 'John');
+
+        $this->assertSame('John', $user->name);
+        $this->assertSame(18, $user->age);
+        $this->assertEmpty($user->favorite_movies);
+    }
+
+    public function testAddFavoriteMovie()
+    {
+        $user = new User(18, 'John');
+
+        $this->assertTrue($user->addFavoriteMovie('Avengers'));
+        $this->assertContains('Avengers', $user->favorite_movies);
+        $this->assertCount(1, $user->favorite_movies);
+    }
+
+    public function testRemoveFavoriteMovie()
+    {
+        $user = new User(18, 'John');
+
+        $this->assertTrue($user->addFavoriteMovie('Avengers'));
+        $this->assertTrue($user->addFavoriteMovie('Justice League'));
+
+        $this->assertTrue($user->removeFavoriteMovie('Avengers'));
+        $this->assertNotContains('Avengers', $user->favorite_movies);
+        $this->assertCount(1, $user->favorite_movies);
+    }
+}
