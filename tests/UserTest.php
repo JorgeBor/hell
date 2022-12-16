b062f993dfc5d0281ec332ba7c87e4082d9b5471
Jorge Borgos <jorge__b@hotmail.com>
2022-12-16 16:22:13 -0500

    fixed testing error

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -7,7 +7,7 @@ final class UserTest extends TestCase
 {
     public function testClassConstructor()
     {
-        $user = new User(18, 'Ken');
+        $user = new User(18, 'John');
 
         $this->assertSame('John', $user->name);
         $this->assertSame(18, $user->age);
